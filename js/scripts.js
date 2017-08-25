d0d04a688697e8d75d90915290ff46a72d78d8dd
kr <ktrhx4@mail.missouri.edu>
2017-08-25 12:19:34 -0700

    adjust order and pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -10,23 +10,25 @@ Pizza.prototype.totalPrice = function() {
   return '$' + (((this.size + this.topping) * this.quantity) + this.delivery);
 };
 
+var order = new Pizza ();
+
 // front-end logic
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
     $("ul#receipt").empty(order);
 
-    var inputtedSize = parseInt($('#pizzaSize').val());
-    var inputtedTopping = parseInt($('input[name="topping"]:checked').val());
-    var inputtedDelivery = parseInt($('#pizzaDelivery').val());
-    var inputtedQuantity = parseInt($('#pizzaQuantity').val());
+    this.size = parseInt($('#pizzaSize').val());
+    this.topping = parseInt($('input[name="topping"]:checked').val());
+    this.delivery = parseInt($('#pizzaDelivery').val());
+    this.quantity = parseInt($('#pizzaQuantity').val());
 
     var finalSize = $("#pizzaSize option:selected").text();
     var finalTopping = $('input[name="topping"]:checked').text();
     var finalDelivery = $("#pizzaDelivery option:selected").text();
     var finalQuantity = $("#pizzaQuantity option:selected").text();
 
-    var order = new Pizza(inputtedSize, inputtedTopping, inputtedDelivery, inputtedQuantity);
+    // var order = new Pizza(inputtedSize, inputtedTopping, inputtedDelivery, inputtedQuantity);
 
     $('ul#receipt').append(finalSize + " " + finalTopping + " " + finalDelivery + " " + finalQuantity + " " + order.totalPrice());
   });
