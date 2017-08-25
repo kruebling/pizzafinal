2a25541a85743109fb2f70549faf959fb07c5597
kr <ktrhx4@mail.missouri.edu>
2017-08-25 11:01:25 -0700

    add front end logic to receipt

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,28 @@ function Pizza(size, topping, delivery, quantity) {
   this.quantity = quantity;
 }
 
-Ticket.prototype.totalPrice = function() {
+Pizza.prototype.totalPrice = function() {
   return '$' + ((this.size + this.topping) * this.quantity) + this.delivery;
 };
 
 // front-end logic
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("form#pizza").submit(function(event) {
     event.preventDefault();
-    $("ul#receipt").empty(newTicket);
+    $("ul#receipt").empty(order);
 
-    var inputtedMovie = parseFloat($('#movieName').val());
-    var inputtedTime = parseFloat($('#movieTime').val());
-    var inputtedAge = parseFloat($('#movieAdmission').val());
-    var inputtedQuantity = parseFloat($('#movieQuantity').val());
+    var inputtedSize = parseFloat($('#pizzaSize').val());
+    var inputtedTopping = parseFloat($('input:checkbox[name=topping]:checked').val());
+    var inputtedDelivery = parseFloat($('#delivery').val());
+    var inputtedQuantity = parseFloat($('#pizzaQuantity').val());
 
-    var finalMovie = $("#movieName option:selected").text();
-    var finalTime = $("#movieTime option:selected").text();
-    var finalAge = $("#movieAdmission option:selected").text();
-    var finalQuantity = $("#movieQuantity option:selected").text();
+    var finalSize = $("#pizzaSize option:selected").text();
+    var finalTopping = $("input:checkbox[name=topping]:checked").text();
+    var finalDelivery = $("#delivery option:selected").text();
+    var finalQuantity = $("#pizzaQuantity option:selected").text();
 
-    var newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge, inputtedQuantity);
+    var order = new Pizza(inputtedSize, inputtedTopping, inputtedDelivery, inputtedQuantity);
 
-    $('ul#receipt').append(finalMovie + " " + finalTime + " " + finalAge + " " + finalQuantity + " " + newTicket.totalPrice());
+    $('ul#receipt').append(finalSize + " " + finalTopping + " " + finalDelivery + " " + finalQuantity + " " + order.totalPrice());
   });
 });
