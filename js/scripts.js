99702545c8a64038301124c03b0337e1ca803120
kr <ktrhx4@mail.missouri.edu>
2017-08-25 11:42:14 -0700

    modify input for checked fields

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Pizza(size, topping, delivery, quantity) {
 }
 
 Pizza.prototype.totalPrice = function() {
-  return '$' + ((this.size + this.topping) * this.quantity) + this.delivery;
+  return '$' + (((this.size + this.topping) * this.quantity) + this.delivery);
 };
 
 // front-end logic
@@ -16,14 +16,14 @@ $(document).ready(function() {
     event.preventDefault();
     $("ul#receipt").empty(order);
 
-    var inputtedSize = parseFloat($('#pizzaSize').val());
-    var inputtedTopping = parseFloat($('input:checkbox[name=topping]:checked').val());
-    var inputtedDelivery = parseFloat($('#delivery').val());
-    var inputtedQuantity = parseFloat($('#pizzaQuantity').val());
+    var inputtedSize = parseInt($('#pizzaSize').val());
+    var inputtedTopping = parseInt($('input[name="topping"]:checked').val());
+    var inputtedDelivery = parseInt($('#pizzaDelivery').val());
+    var inputtedQuantity = parseInt($('#pizzaQuantity').val());
 
     var finalSize = $("#pizzaSize option:selected").text();
-    var finalTopping = $("input:checkbox[name=topping]:checked").text();
-    var finalDelivery = $("#delivery option:selected").text();
+    var finalTopping = $('input[name="topping"]:checked').text();
+    var finalDelivery = $("#pizzaDelivery option:selected").text();
     var finalQuantity = $("#pizzaQuantity option:selected").text();
 
     var order = new Pizza(inputtedSize, inputtedTopping, inputtedDelivery, inputtedQuantity);
